b8135f6120c38db930ba762a62eb8eaacc00f9e3
Diogo Machado <diogo@diogomachado.com>
2021-05-08 17:50:40 -0300

    Added enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("test initial page without logged", () => {
+    it('should have <a> link to login', () => {
+        const wrapper = shallow(<App/>);
+        expect(wrapper.find('a')).toHaveLength(1);
+    })
+
+    it('should the link <a> to have a description Sign account', () => {
+        const wrapper = shallow(<App/>);
+        expect(wrapper.find('a').get(0).props.children).toBe("Sign account");
+    })
 });
