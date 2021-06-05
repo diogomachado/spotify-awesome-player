61058ea0ed19bde9173435ad818d70de64b48b9a
Diogo Machado <diogo@diogomachado.com>
2021-06-05 11:03:45 -0300

    Add styles to show info about the current music

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -17,7 +17,22 @@ export default function Footer() {
         <footer className="footer">
             <Progress value={valueProgress} />
             <div className="footer-container">
+
+                {/* Details about the music playing */}
+                <aside className="music-info">
+                    <div className="music-info-cover">
+                        <img src="https://i1.sndcdn.com/artworks-000527167749-vpxrk0-t500x500.jpg" />
+                    </div>
+                    <div className="music-info-details">
+                        <h3 className="music-info-details-song">Não é sério</h3>
+                        <p className="music-info-details-artist">Charlie Brown Jr.</p>
+                    </div>
+                </aside>
+
                 <Controls />
+
+                {/* Div only to alignment works */}
+                <div className="align-div"></div>
             </div>
         </footer>
     );
